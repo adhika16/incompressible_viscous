959e039357c704976cb1e679db1a3ba26737123e
adhika <villaadhika@gmail.com>
2019-05-05 15:41:26 +0700

    update global data

--- global.cpp
+++ global.cpp
@@ -5,4 +5,10 @@ namespace Pars
 	extern const double miu = 1.0e-5;
 	extern const double rho = 1.226;
 	extern const double p_atm = 1.0e5;
+	extern const double lx = 1;
+	extern const double ly = 1;
+	extern const int nx = 50;
+	extern const int ny = 50;
+	extern const double dx = lx/(double)nx;
+	extern const double dy = ly/(double)ny;
 }
--- global.hpp
+++ global.hpp
@@ -15,6 +15,12 @@ namespace Pars
 	extern const double miu;
 	extern const double rho;
 	extern const double p_atm;
+	extern const double lx;
+	extern const double ly;
+	extern const int nx;
+	extern const int ny;
+	extern const double dx;
+	extern const double dy;
 }
 
 #endif
