fd3d1c047a659d07ddd198a60fc6a96df0738630
adhika <villaadhika@gmail.com>
2019-05-05 12:49:54 +0700

    add global variables

---
+++ global.cpp
@@ -0,0 +1,8 @@
+#include "global.hpp"
+
+namespace Pars
+{
+	extern const miu = 1.0e-5;
+	extern const rho = 1.226;
+	extern const p_atm = 1.0e5;
+}
---
+++ global.hpp
@@ -0,0 +1,20 @@
+#ifndef INCLUDED_GLOBAL
+#define INCLUDED_GLOBAL
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+namespace Pars
+{
+	extern const miu;
+	extern const rho;
+	extern const p_atm;
+}
+
+#endif
