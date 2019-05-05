f89c6cdb0e8d91393960a0404009a3d962ef3943
adhika <villaadhika@gmail.com>
2019-05-05 15:41:48 +0700

    update main program

--- main.cpp
+++ main.cpp
@@ -2,21 +2,27 @@
 #include "global.hpp"
 #endif
 
+#ifndef INCLUDED_INITIALIZATION
+#include "src/initialization.hpp"
+#endif
+
 int main(int argc, char const *argv[])
 {
 	// == dictionary
 	std::vector<std::vector<double>> x_IJ; // x coordinate to store pressure
 	std::vector<std::vector<double>> y_IJ; // y coordinate to store pressure
-	std::vector<std::vector<double>> x_ij_u;  // x coordinate of forward staggered gird to store u
-	std::vector<std::vector<double>> y_ij_u;  // y coordinate of forward staggered gird to store u
-	std::vector<std::vector<double>> x_ij_v;  // x coordinate of forward staggered gird to store v
-	std::vector<std::vector<double>> y_ij_v;  // y coordinate of forward staggered gird to store v
+	std::vector<std::vector<double>> x_ij_u;  // x coordinate of forward staggered grid to store u
+	std::vector<std::vector<double>> y_ij_u;  // y coordinate of forward staggered grid to store u
+	std::vector<std::vector<double>> x_ij_v;  // x coordinate of forward staggered grid to store v
+	std::vector<std::vector<double>> y_ij_v;  // y coordinate of forward staggered grid to store v
 
 	// === main ===
 	printf("\n\t=================================================");
 	printf("\n\t====== Incompressible Viscous Flows Solver ======");
 	printf("\n\t=================================================\n");
 	// == initialization ==
+	Initialization _Initialization;
+	_Initialization.init_domain(x_IJ, y_IJ, x_ij_u, y_ij_u, x_ij_v, y_ij_v);
 	/*
 	your code here ...
 	*/
