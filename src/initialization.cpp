acaa53199b4ca57b5f9d32cf1e73363cd4965d1f
adhika <villaadhika@gmail.com>
2019-05-05 15:41:11 +0700

    add initialization procedure

---
+++ src/initialization.cpp
@@ -0,0 +1,97 @@
+#include "initialization.hpp"
+
+// =====================================================
+// =====================================================
+Initialization::Initialization()
+{
+	this->_nx = Pars::nx;
+  this->_ny = Pars::ny;
+  this->_lx = Pars::lx;
+  this->_ly = Pars::ly;
+  this->_dx = Pars::dx;
+  this->_dy = Pars::dy;
+}
+// =====================================================
+// =====================================================
+Initialization::~Initialization()
+{
+
+}
+// =====================================================
+// =====================================================
+void Initialization::init_domain(std::vector<std::vector<double>> x_IJ, std::vector<std::vector<double>> y_IJ,
+ std::vector<std::vector<double>> x_ij_u, std::vector<std::vector<double>> y_ij_u,
+ std::vector<std::vector<double>> x_ij_v, std::vector<std::vector<double>> y_ij_v)
+ {
+   x_IJ.resize(_nx, std::vector<double>(_ny));
+   y_IJ.resize(_nx, std::vector<double>(_ny));
+   x_ij_u.resize(_nx-1, std::vector<double>(_ny));
+   y_ij_u.resize(_nx-1, std::vector<double>(_ny));
+   x_ij_v.resize(_nx, std::vector<double>(_ny-1));
+   y_ij_v.resize(_nx, std::vector<double>(_ny-1));
+
+   // == saving data ==
+   std::ofstream outx;
+   std::ofstream outy;
+   std::ofstream outs;
+
+   // -- setup for P domain
+   outx.open("output/x_IJ.csv");
+   outy.open("output/y_IJ.csv");
+   outs.open("output/IJ.csv");
+   for (size_t i = 0; i < _nx; i++) {
+     for (size_t j = 0; j < _ny; j++) {
+       x_IJ[i][j] = 0.0e0 + (double)i/((double)_nx-1)*_lx;
+       y_IJ[i][j] = 0.0e0 + (double)j/((double)_ny-1)*_ly;
+  		 outx << x_IJ[i][j] << ",";
+       outy << y_IJ[i][j] << ",";
+       outs << x_IJ[i][j] << "," << y_IJ[i][j] << "\n";
+     }
+     outx << "\n";
+     outy << "\n";
+   }
+   outx.close();
+   outy.close();
+   outs.close();
+  // -- setup for u domain, forward staggered
+  outx.open("output/x_ij_u.csv");
+  outy.open("output/y_ij_u.csv");
+  outs.open("output/ij_u.csv");
+   for (size_t i = 0; i < _nx-1; i++) {
+     for (size_t j = 0; j < _ny; j++) {
+       x_ij_u[i][j] = x_IJ[i][j] + 0.5*_dx;
+       y_ij_u[i][j] = y_IJ[i][j];
+       outx << x_ij_u[i][j] << ",";
+       outy << y_ij_u[i][j] << ",";
+       outs << x_ij_u[i][j] << "," << y_ij_u[i][j] << "\n";
+     }
+     outx << "\n";
+     outy << "\n";
+   }
+   outx.close();
+   outy.close();
+   outs.close();
+  // -- setup for v domain, forward staggered
+  outx.open("output/x_ij_v.csv");
+  outy.open("output/y_ij_v.csv");
+  outs.open("output/ij_v.csv");
+   for (size_t i = 0; i < _nx; i++) {
+     for (size_t j = 0; j < _ny-1; j++) {
+       x_ij_v[i][j] = x_IJ[i][j];
+       y_ij_v[i][j] = y_IJ[i][j] + 0.5*_dy;
+       outx << x_ij_v[i][j] << ",";
+       outy << y_ij_v[i][j] << ",";
+       outs << x_ij_v[i][j] << "," << y_ij_v[i][j] << "\n";
+     }
+     outx << "\n";
+     outy << "\n";
+   }
+   outx.close();
+   outy.close();
+   outs.close();
+ 	// outs.open("output/IJ.csv");
+ 	// for (int i = 0; i < x.size(); i++)
+ 	// 	outs << x[i] << " " << y[i] << "\n";
+ 	// outs.close();
+
+ }
---
+++ src/initialization.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDED_INITIALIZATION
+#define INCLUDED_INITIALIZATION
+
+#ifndef INCLUDED_GLOBAL
+#include "../global.hpp"
+#endif
+
+// #include <locale> // std::toupper, std::tolower
+
+class Initialization
+{
+  int _nx;
+  int _ny;
+	double _lx;
+  double _ly;
+  double _dx;
+  double _dy;
+
+public:
+	Initialization();
+
+  void init_domain(std::vector<std::vector<double>> x_IJ, std::vector<std::vector<double>> y_IJ,
+   std::vector<std::vector<double>> x_ij_u, std::vector<std::vector<double>> y_ij_u,
+   std::vector<std::vector<double>> x_ij_v, std::vector<std::vector<double>> y_ij_v);
+
+	~Initialization();
+
+};
+
+
+#endif
